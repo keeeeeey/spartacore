cbf006adc8aedb5739df040072b81ab0f2f19d0c
keeeeeey <sseioul@naver.com>
2022-02-12 22:08:04 +0900

    .

--- src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
+++ src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
@@ -12,16 +12,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
+                // image 폴더를 login 없이 허용
+                .antMatchers("/images/**").permitAll()
+                // css 폴더를 login 없이 허용
+                .antMatchers("/css/**").permitAll()
                 // 어떤 요청이든 '인증'
                 .anyRequest().authenticated()
                 .and()
-                    // 로그인 기능 허용
-                    .formLogin()
-                    .defaultSuccessUrl("/")
-                    .permitAll()
+                // 로그인 기능 허용
+                .formLogin()
+                .loginPage("/user/login")
+                .defaultSuccessUrl("/")
+                .failureUrl("/user/login?error")
+                .permitAll()
                 .and()
-                    // 로그아웃 기능 허용
-                    .logout()
-                    .permitAll();
+                // 로그아웃 기능 허용
+                .logout()
+                .permitAll();
     }
 }
