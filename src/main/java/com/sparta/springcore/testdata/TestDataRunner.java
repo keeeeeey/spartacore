91536a5ec80be5071542c1fdc65480fb8c1b1c46
keeeeeey <sseioul@naver.com>
2022-03-02 16:54:28 +0900

    AOP 기능 결합

--- src/main/java/com/sparta/springcore/testdata/TestDataRunner.java
+++ src/main/java/com/sparta/springcore/testdata/TestDataRunner.java
@@ -40,43 +40,47 @@ public class TestDataRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-// 테스트 User 생성
-        User testUser = new User("슈가", passwordEncoder.encode("123"), "sugar@sparta.com", UserRoleEnum.USER);
-        testUser = userRepository.save(testUser);
+        // 테스트 User 생성
+        User testUser1 = new User("정국", passwordEncoder.encode("123"), "jg@sparta.com", UserRoleEnum.USER);
+        User testUser2 = new User("제이홉", passwordEncoder.encode("123"), "hope@sparta.com", UserRoleEnum.USER);
+        User testAdminUser1 = new User("아미", passwordEncoder.encode("123"), "army@sparta.com", UserRoleEnum.ADMIN);
+        testUser1 = userRepository.save(testUser1);
+        testUser2 = userRepository.save(testUser2);
+        testAdminUser1 = userRepository.save(testAdminUser1);
 
-// 테스트 User 의 관심상품 등록
-// 검색어 당 관심상품 10개 등록
-        createTestData(testUser, "신발");
-        createTestData(testUser, "과자");
-        createTestData(testUser, "키보드");
-        createTestData(testUser, "휴지");
-        createTestData(testUser, "휴대폰");
-        createTestData(testUser, "앨범");
-        createTestData(testUser, "헤드폰");
-        createTestData(testUser, "이어폰");
-        createTestData(testUser, "노트북");
-        createTestData(testUser, "무선 이어폰");
-        createTestData(testUser, "모니터");
+        // 테스트 User 의 관심상품 등록
+        // 검색어 당 관심상품 10개 등록
+        createTestData(testUser1, "신발");
+        createTestData(testUser1, "과자");
+        createTestData(testUser1, "키보드");
+        createTestData(testUser1, "휴지");
+        createTestData(testUser1, "휴대폰");
+        createTestData(testUser1, "앨범");
+        createTestData(testUser1, "헤드폰");
+        createTestData(testUser1, "이어폰");
+        createTestData(testUser1, "노트북");
+        createTestData(testUser1, "무선 이어폰");
+        createTestData(testUser1, "모니터");
     }
 
     private void createTestData(User user, String searchWord) throws IOException {
-// 네이버 쇼핑 API 통해 상품 검색
+        // 네이버 쇼핑 API 통해 상품 검색
         List<ItemDto> itemDtoList = itemSearchService.getItems(searchWord);
 
         List<Product> productList = new ArrayList<>();
 
         for (ItemDto itemDto : itemDtoList) {
             Product product = new Product();
-// 관심상품 저장 사용자
+            // 관심상품 저장 사용자
             product.setUserId(user.getId());
-// 관심상품 정보
+            // 관심상품 정보
             product.setTitle(itemDto.getTitle());
             product.setLink(itemDto.getLink());
             product.setImage(itemDto.getImage());
             product.setLprice(itemDto.getLprice());
 
-// 희망 최저가 랜덤값 생성
-// 최저 (100원) ~ 최대 (상품의 현재 최저가 + 10000원)
+            // 희망 최저가 랜덤값 생성
+            // 최저 (100원) ~ 최대 (상품의 현재 최저가 + 10000원)
             int myPrice = getRandomNumber(MIN_MY_PRICE, itemDto.getLprice() + 10000);
             product.setMyprice(myPrice);
 
