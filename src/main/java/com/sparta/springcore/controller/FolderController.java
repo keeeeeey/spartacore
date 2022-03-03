6e3c505a3bfc719d80403e54ec90da7c1e21cdf0
keeeeeey <sseioul@naver.com>
2022-03-03 23:35:00 +0900

    Global ExceptionHandler 구현

--- src/main/java/com/sparta/springcore/controller/FolderController.java
+++ src/main/java/com/sparta/springcore/controller/FolderController.java
@@ -1,7 +1,6 @@
 package com.sparta.springcore.controller;
 
 import com.sparta.springcore.dto.FolderRequestDto;
-import com.sparta.springcore.exception.RestApiException;
 import com.sparta.springcore.model.Folder;
 import com.sparta.springcore.model.Product;
 import com.sparta.springcore.model.User;
@@ -9,8 +8,6 @@ import com.sparta.springcore.security.UserDetailsImpl;
 import com.sparta.springcore.service.FolderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -62,19 +59,6 @@ public class FolderController {
                 sortBy,
                 isAsc,
                 userDetails.getUser()
-        );
-    }
-
-    @ExceptionHandler({ IllegalArgumentException.class })
-    public ResponseEntity handleException(IllegalArgumentException ex) {
-        RestApiException restApiException = new RestApiException();
-        restApiException.setHttpStatus(HttpStatus.BAD_REQUEST);
-        restApiException.setErrorMessage(ex.getMessage());
-        return new ResponseEntity(
-// HTTP body
-                restApiException,
-// HTTP status code
-                HttpStatus.BAD_REQUEST
         );
     }
 }
